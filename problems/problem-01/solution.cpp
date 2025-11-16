b7b19c01c11b6965a24462130073bcf59fd09347
Orange X <alex.xu202208@gmail.com>
2025-11-16 07:57:43 -0800

    Add solution to calculate sum of array elements
    Implement a solution to calculate the sum of an array.

---
+++ problems/problem-01/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // For testing - UNCOMMENT the line below
+    // freopen("input.txt", "r", stdin);
+    
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    // Your solution here
+    int sum = 0;
+    for (int num : arr) sum += num;
+    cout << "Sum: " << sum << endl;
+    
+    return 0;
+}
